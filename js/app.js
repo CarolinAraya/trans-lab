083441412dff8aa56e4e6da972a218f7b9eaa1b2
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-06-27 10:50:18 -0400

    comments and clusters function

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
 const btn = document.querySelector("button");
 const name = document.getElementById("name");
-const mail = document.getElementById("mail")
+const mail = document.getElementById("mail");
+const password = document.getElementById("password");
 
-fetch(`http://www.psep.cl/api/Bip.php?&numberBip=666`) //sin ; después de fech
+const loadData = () => {
+    fetch(`http://www.psep.cl/api/Bip.php?&numberBip=666`) 
     .then(response => response.json())
     .then(data => {
         let stateBip = data;
         console.log(data);
     })
 
+}
 
-
-
-const validar = (name, mail) => {
-    if (mail && name) {
-
-    }
+const validate = () => {//validation of user data in imput
+    const password = () => {//imput type number
+        if(password.length > 8){
+            //little mesage in red after button "iniciar sesión", css
+        }
+    };
+    
+    
+    const introduceUser = (name, mail) => {
+        if (mail && name) {
+    
+        }
+    };
 };
+
